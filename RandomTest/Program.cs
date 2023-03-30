087823ef346ac3ab89a6a67aeb1ee07318fbb3ee
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-30 16:07:59 +0100

    Altered r1 and r2 values to test different results

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -6,8 +6,8 @@ namespace RandomTest
     {
         private static void Main(string[] args)
         {
-            Random r1 = new Random(22);
-            Random r2 = new Random(22);
+            Random r1 = new Random(24);
+            Random r2 = new Random(18);
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r1.Next());
             Console.WriteLine();
