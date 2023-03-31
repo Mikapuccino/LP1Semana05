10d91118899abce1b3a486e21c8a765917cbc920
Nelson <nelsonfcmilheiro@gmail.com>
2023-03-31 13:31:28 +0100

    Add comments to program

--- LowerUnion/Program.cs
+++ LowerUnion/Program.cs
@@ -6,19 +6,23 @@ namespace LowerUnion
     {
         static void Main(string[] args)
         {
+            // If there are no arguments, prints message and ends program
             if (args.Length == 0)
             {
                 Console.WriteLine("sem qualquer tipo de argumento!");
                 return;
             }
             
+            // Puts every string in args into lower case
             for (int i = 0; i < args.Length; i++)
             {
                 args[i] = args[i].ToLower();
             }
 
+            // Sorts every argument in alphabetical order
             Array.Sort(args);
 
+            // Merges every argument into one string and writes it on console
             string argMerge = string.Join("_", args);
 
             Console.WriteLine(argMerge);
