bb0f9d69abd8b22e81ad3546292eb0f047e5c397
Nelson <nelsonfcmilheiro@gmail.com>
2023-03-31 13:27:31 +0100

    Arguments merge in one string and that string is written on console

--- LowerUnion/Program.cs
+++ LowerUnion/Program.cs
@@ -6,10 +6,22 @@ namespace LowerUnion
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("sem qualquer tipo de argumento!");
+                return;
+            }
+            
             for (int i = 0; i < args.Length; i++)
             {
                 args[i] = args[i].ToLower();
             }
+
+            Array.Sort(args);
+
+            string argMerge = string.Join("_", args);
+
+            Console.WriteLine(argMerge);
         }
     }
 }
